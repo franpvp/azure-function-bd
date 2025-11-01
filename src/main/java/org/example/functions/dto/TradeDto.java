eb3851bf1a6bf1e6f7c999b5f77c2ff0a6404cff
franpvp <franpvp.98@gmail.com>
2025-11-01 00:39:14 -0300

    Se agrega nueva columna a TradeDto

--- src/main/java/org/example/functions/dto/TradeDto.java
+++ src/main/java/org/example/functions/dto/TradeDto.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 public class TradeDto {
     private Long idTrade;
     private Long monto;
+    private String canal;
     private LocalDate fechaCreacion;
     private Long idCliente;
 }
